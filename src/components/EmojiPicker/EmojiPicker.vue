17e8c60aa5931bc17e8a4e423c84c4e5146bf771
maksimkokhtenko <maxkohtenko@gmail.com>
2024-05-22 17:48:54 +0400

    add search and frequently used emojis

--- src/components/EmojiPicker/EmojiPicker.vue
+++ src/components/EmojiPicker/EmojiPicker.vue
@@ -5,11 +5,12 @@
         <div class="emojipicker__inner">
             <span class="emojipicker__icon" @click.prevent="togglePicker">😊</span>
             <div class="emojipicker__container" :class="{ open: isOpen }">
-                <div v-for="category in categories" :key="category" class="emojipicker__category">
-                    <span>{{ category }}</span>
+                <input ref="search" type="text" v-model="searchQuery" placeholder="Search emojis..."/>
+                <div v-for="category in displayCategories" :key="category" class="emojipicker__category">
+                    <span v-if="category !== 'Frequently used' || hasFrequentlyUsedEmojis">{{ category }}</span>
                     <div class="emojipicker__emojis">
                         <button
-                            v-for="(emoji, index) in emojis[category]"
+                            v-for="(emoji, index) in filteredEmojis[category]"
                             :key="index"
                             @click.prevent="handleEmojiClick(emoji)"
                         >
@@ -23,21 +24,75 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import emojis from './emojis.json'
 
 const input = ref(null)
+const search = ref(null)
 const isOpen = ref(false)
+const searchQuery = ref('')
+const frequentlyUsed = ref(JSON.parse(localStorage.getItem('frequentlyUsed')) || {})
 
 const categories = computed(() => Object.keys(emojis))
+
+const hasFrequentlyUsedEmojis = computed(() => frequentlyUsed.value['Frequently used'] && frequentlyUsed.value['Frequently used'].length > 0)
+
+const filteredEmojis = computed(() => {
+    const query = searchQuery.value.toLowerCase()
+    const result = {}
+
+    if (hasFrequentlyUsedEmojis.value) {
+        result['Frequently used'] = frequentlyUsed.value['Frequently used']
+    }
+
+    categories.value.forEach(category => {
+        const filtered = Object.entries(emojis[category]).filter(([name, emoji]) =>
+            name.toLowerCase().includes(query) || emoji.includes(query)
+        )
+
+        if (filtered.length > 0) {
+            result[category] = Object.fromEntries(filtered)
+        }
+    })
+
+    return result
+})
+
+const displayCategories = computed(() => {
+    const categoriesToShow = hasFrequentlyUsedEmojis.value ? ['Frequently used', ...categories.value] : categories.value
+    return categoriesToShow.filter(category => filteredEmojis.value[category] && Object.keys(filteredEmojis.value[category]).length > 0)
+})
 
 const handleEmojiClick = emoji => {
     input.value.value += emoji
     input.value.focus()
     isOpen.value = false
+    updateFrequentlyUsed(emoji)
+}
+
+const updateFrequentlyUsed = emoji => {
+    const frequentlyUsedEmojis = frequentlyUsed.value['Frequently used'] || []
+    const emojiIndex = frequentlyUsedEmojis.indexOf(emoji)
+
+    if (emojiIndex !== -1) {
+        frequentlyUsedEmojis.splice(emojiIndex, 1)
+    }
+
+    frequentlyUsedEmojis.unshift(emoji)
+
+    if (frequentlyUsedEmojis.length > 10) {
+        frequentlyUsedEmojis.pop()
+    }
+
+    frequentlyUsed.value['Frequently used'] = frequentlyUsedEmojis
+    localStorage.setItem('frequentlyUsed', JSON.stringify(frequentlyUsed.value))
 }
 
 const togglePicker = () => {
     isOpen.value = !isOpen.value
 }
+
+onMounted(() => {
+    frequentlyUsed.value = JSON.parse(localStorage.getItem('frequentlyUsed')) || {}
+})
 </script>
